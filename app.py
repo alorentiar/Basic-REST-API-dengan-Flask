c8ca4719c1357e39cbb8ba5bb980e4e20f93f14a
alorentiar <alorentiar03@gmail.com>
2021-12-21 23:43:17 +0700

    Create & Read data using flask and SQLAlchemy

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS 
 
+#import lib sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 #inisiasi object
 app = Flask(__name__)
 
@@ -12,8 +16,35 @@ api = Api(app)
 #inisiasi object flask_cors
 CORS(app)
 
+#inisiasi object flask sqlalchemy
+db = SQLAlchemy(app)
+
+#config database 
+basedir = os.path.dirname(os.path.abspath(__file__))
+database = "sqlite:///" + os.path.join(basedir,"db.sqlite")
+app.config["SQLALCHEMY_DATABASE_URI"] = database
+
+# Membuat database model
+class ModelDatabase(db.Model):
+    #membuat field/kolom
+    id = db.Column(db.Integer,primary_key=True)
+    nama = db.Column(db.String(100))
+    umur = db.Column(db.Integer)
+    alamat = db.Column(db.TEXT)
+
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return True
+        except:
+            return False
+
 #inisiasi variabel kosong bertype dictionary
 identitas = {} #global variable, dictionary = json
+
+#create database
+db.create_all()
 
 
 #membuat class Resource 
@@ -21,14 +52,40 @@ class ContohResource(Resource):
         #metode get dan post
         def get(self):
             #response = {"msg":"Halo semua, ini ujicoba restful pertamaku"}
-            return identitas
+            #menampilkan data dari database sqlite
+            query = ModelDatabase.query.all()
 
+            #loop pada model database
+            identitas = [
+                {
+                    "nama":data.nama, 
+                    "umur":data.umur,
+                    "alamat":data.alamat
+                } 
+                for data in query
+            ]
+
+            response    = {
+                "Code" : 200,
+                "msg"  : "Query data sukses",
+                "data" : identitas
+            }
+            return identitas, 200
+
+        #method mmenyimpan data
         def post(self):
-            nama = request.form["nama"]
-            umur = request.form["umur"]
-            identitas["nama"] = nama
-            identitas["umur"] = umur
-            response = {"msg" : "Sukses memasukkan data"}
+            dataNama = request.form["nama"]
+            dataUmur = request.form["umur"]
+            dataAlamat = request.form["alamat"]
+            
+            #masukkan data ke database model
+            model = ModelDatabase(nama=dataNama,umur=dataUmur,alamat=dataAlamat)
+            model.save()
+
+            response = {
+                "msg" : "Data berhasil dimasukkan",
+                "code" : 200
+            }
             return response
             
 #setup resource
